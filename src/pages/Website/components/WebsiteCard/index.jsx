8b58decdc6cf6528df3b7ed074b5ac20517c6e3b
LanM <xtwang@sudytech.cn>
2023-02-06 15:14:53 +0800

    2

--- src/pages/Website/components/WebsiteCard/index.jsx
+++ src/pages/Website/components/WebsiteCard/index.jsx
@@ -1,41 +1,41 @@
-import React, { memo } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
+// import React, { memo } from "react";
+// import clsx from "clsx";
+// import Link from "@docusaurus/Link";
 
-import styles from "./styles.module.css";
-// import { type Website } from '@site/data/website';
-// import Tooltip from '../../../project/_components/ShowcaseTooltip';
+// import styles from "./styles.module.css";
+// // import { type Website } from '@site/data/website';
+// // import Tooltip from '../../../project/_components/ShowcaseTooltip';
 
-const WebsiteCard = memo(({ website }) => (
-  <li
-    key={website.name}
-    className={clsx(styles.websiteCard, "padding-vert--sm padding-horiz--md")}
-  >
-    <img
-      src={
-        typeof website.logo === "string" ? website.logo : website.logo?.src?.src
-      }
-      alt={website.name}
-      className={clsx(styles.websiteCardImage)}
-    />
-    <div className={styles.websiteCardBody}>
-      <div className={clsx(styles.websiteCardHeader)}>
-        <h4 className={styles.websiteCardTitle}>
-          <Link href={website.href} className={styles.websiteCardLink}>
-            {website.name}
-          </Link>
-        </h4>
-      </div>
-      {/* <Tooltip
-        key={website.name}
-        text={website.desc}
-        anchorEl="#__docusaurus"
-        id={`tooltip-${website.name}`}
-      >
-        <p className={styles.websiteCardDesc}>{website.desc}</p>
-      </Tooltip> */}
-    </div>
-  </li>
-));
+// const WebsiteCard = memo(({ website }) => (
+//   <li
+//     key={website.name}
+//     className={clsx(styles.websiteCard, "padding-vert--sm padding-horiz--md")}
+//   >
+//     <img
+//       src={
+//         typeof website.logo === "string" ? website.logo : website.logo?.src?.src
+//       }
+//       alt={website.name}
+//       className={clsx(styles.websiteCardImage)}
+//     />
+//     <div className={styles.websiteCardBody}>
+//       <div className={clsx(styles.websiteCardHeader)}>
+//         <h4 className={styles.websiteCardTitle}>
+//           <Link href={website.href} className={styles.websiteCardLink}>
+//             {website.name}
+//           </Link>
+//         </h4>
+//       </div>
+//       {/* <Tooltip
+//         key={website.name}
+//         text={website.desc}
+//         anchorEl="#__docusaurus"
+//         id={`tooltip-${website.name}`}
+//       >
+//         <p className={styles.websiteCardDesc}>{website.desc}</p>
+//       </Tooltip> */}
+//     </div>
+//   </li>
+// ));
 
-export default WebsiteCard;
+// export default WebsiteCard;
