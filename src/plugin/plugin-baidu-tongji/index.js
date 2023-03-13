89cd6c5e009736fe1cf5219c6bac67dd9e579eab
BlueLanM <100191779+BlueLanM@users.noreply.github.com>
2023-03-13 17:29:32 +0800

    update: baidu-tongji

--- src/plugin/plugin-baidu-tongji/index.js
+++ src/plugin/plugin-baidu-tongji/index.js
@@ -1,27 +1,26 @@
 module.exports = function (context, options) {
   return {
-    name: 'docusaurus-plugin-baidu-tongji',
+    name: "docusaurus-plugin-baidu-tongji",
     injectHtmlTags() {
       return {
         headTags: [
           {
-            tagName: 'script',
+            tagName: "script",
             innerHTML: `
             var _hmt = _hmt || [];
             (function() {
               var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?c9a3849aa75f9c4a4e65f846cd1a5155";
-              hm.defer = true;
-              var s = document.getElementsByTagName("script")[0];
+              hm.src = "https://hm.baidu.com/hm.js?7bc87de561fa789f1285707faed5853e";
+              var s = document.getElementsByTagName("script")[0]; 
               s.parentNode.insertBefore(hm, s);
             })();
           `,
           },
           {
-            tagName: 'meta',
+            tagName: "meta",
             attributes: {
-              name: 'baidu-site-verification',
-              content: 'code-rqLUw5reVS',
+              name: "baidu-site-verification",
+              content: "code-rqLUw5reVS",
             },
           },
         ],
