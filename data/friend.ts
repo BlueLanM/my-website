28551d6d2eb1e8f24a012e6e1cc041c082c455e6
LanM <wxt01765220@alibaba-inc.com>
2023-08-09 10:30:26 +0800

    feat:friend

--- data/friend.ts
+++ data/friend.ts
@@ -5,6 +5,12 @@ export const Friends: Friend[] = [
     website: "https://blog.lkurococ.top/",
     avatar: "https://cdn.jsdelivr.net/gh/6isixi/imgcloud/avatar.jpg",
   },
+  {
+    title: "matto",
+    description: "天天摸鱼身体棒~",
+    website: "https://www.matto.top",
+    avatar: "https://avatars.githubusercontent.com/u/54560462?v=4",
+  },
 ];
 
 export type Friend = {
