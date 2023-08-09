2ed4ec1cb152c169df818bb2930ea99c984b1a7d
LanM <wxt01765220@alibaba-inc.com>
2023-08-09 10:11:10 +0800

    feat: friend

--- data/friend.ts
+++ data/friend.ts
@@ -1,5 +1,10 @@
 export const Friends: Friend[] = [
-  
+  {
+    title: "Kurococ Liu",
+    description: "被子外面很危险！",
+    website: "https://blog.lkurococ.top/",
+    avatar: "https://cdn.jsdelivr.net/gh/6isixi/imgcloud/avatar.jpg",
+  },
 ];
 
 export type Friend = {
