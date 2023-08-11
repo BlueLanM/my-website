f2cadfa2d2fd7c1e606ea6c68aec126ae67e4f60
LanM <wxt01765220@alibaba-inc.com>
2023-08-11 10:48:07 +0800

    style: tranlate code

--- src/pages/notebook/index.tsx
+++ src/pages/notebook/index.tsx
@@ -6,18 +6,9 @@ import styles from "./index.module.scss";
 
 import NoteHeader from "./component/NoteHeader";
 
-const TITLE = translate({
-  id: "theme.friend.title",
-  message: "左邻右舍",
-});
-const DESCRIPTION = translate({
-  id: "theme.friend.description",
-  message: "海内存知己，天涯若比邻",
-});
-
 function NoteBook() {
   return (
-    <Layout title={TITLE} description={DESCRIPTION}>
+    <Layout>
       <main className="margin-vert--lg">
         <div className={styles.container}>
           <NoteHeader />
