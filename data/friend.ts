4ed1e58bce1d5e53258c19b2cbd6f1e0b4d5def6
BlueLanM <100191779+BlueLanM@users.noreply.github.com>
2023-08-28 10:59:29 +0800

    add friend

--- data/friend.ts
+++ data/friend.ts
@@ -5,18 +5,18 @@ export const Friends: Friend[] = [
     website: "https://blog.lkurococ.top/",
     avatar: "https://cdn.jsdelivr.net/gh/6isixi/imgcloud/avatar.jpg",
   },
-  // {
-  //   title: "matto",
-  //   description: "天天摸鱼身体棒~",
-  //   website: "https://www.matto.top",
-  //   avatar: "https://avatars.githubusercontent.com/u/54560462?v=4",
-  // },
   {
-    title: "freejishu的美丽世界",
-    description: "A New World",
-    website: "http://www.freejishu.com",
-    avatar: "https://cdn-p.freejishu.com/img/2022/01/19/gNxI.jpg",
+    title: "matto",
+    description: "天天摸鱼身体棒~",
+    website: "https://www.matto.top",
+    avatar: "https://avatars.githubusercontent.com/u/54560462?v=4",
   },
+  // {
+  //   title: "freejishu的美丽世界",
+  //   description: "A New World",
+  //   website: "http://www.freejishu.com",
+  //   avatar: "https://cdn-p.freejishu.com/img/2022/01/19/gNxI.jpg",
+  // },
   {
     title: "清絮的博客",
     description: "人生如逆旅，我亦是行人",
