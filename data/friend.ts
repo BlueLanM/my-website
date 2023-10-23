1d55136ee57998f53f41a2c5c7eb33fedb56f15c
BlueLanM <w2351406802@163.com>
2023-10-23 09:49:02 +0800

    add friend

--- data/friend.ts
+++ data/friend.ts
@@ -11,12 +11,12 @@ export const Friends: Friend[] = [
     website: "https://www.matto.top",
     avatar: "https://avatars.githubusercontent.com/u/54560462?v=4",
   },
-  // {
-  //   title: "freejishu的美丽世界",
-  //   description: "A New World",
-  //   website: "http://www.freejishu.com",
-  //   avatar: "https://cdn-p.freejishu.com/img/2022/01/19/gNxI.jpg",
-  // },
+  {
+    title: "freejishu的美丽世界",
+    description: "A New World",
+    website: "http://www.freejishu.com",
+    avatar: "https://cdn-p.freejishu.com/img/2022/01/19/gNxI.jpg",
+  },
   {
     title: "清絮的博客",
     description: "人生如逆旅，我亦是行人",
