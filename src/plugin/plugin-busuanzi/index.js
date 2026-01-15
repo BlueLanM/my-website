1816e955190aa0efaf827a225a26a3544713ee02
BlueLanM <100191779+BlueLanM@users.noreply.github.com>
2026-01-15 15:37:14 +0800

    update: 刷新统计数据

--- src/plugin/plugin-busuanzi/index.js
+++ src/plugin/plugin-busuanzi/index.js
@@ -6,8 +6,8 @@ module.exports = function(context, options) {
 				postBodyTags: [
 					{
 						attributes: {
-							defer: true,
-							src: "//cdn.busuanzi.cc/busuanzi/3.6.9/busuanzi.min.js"
+							async: true,
+							src: "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"
 						},
 						tagName: "script"
 					}
